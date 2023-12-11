daed47e6c3708b2fc8cec29f3d38053d598240b7
Artem Shakun <artem.shakun.91@gmail.com>
2023-12-11 14:53:56 +0200

    new test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { List } from '../List/List';
 import './App.css';
 import { Search } from '../Search/Search';
 
-const data = ['HTML', 'CSS', 'JS', 'React', 'TypeScript', 'NodeJs', 'Angular'];
+const data = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'TypeScript',
+  'NodeJs',
+  'Angular',
+];
 
 function App() {
   const [search, setSearch] = useState('');
+  const [items, setItems] = useState(data);
 
+  useEffect(() => {
+    setItems(
+      data.filter(el =>
+        el.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+      ),
+    );
+  }, [search]);
   return (
     <div className="App">
       <div className="App-header">
         <Search value={search} onChange={e => setSearch(e.target.value)}>
           Find course:
         </Search>
-        <List items={data} />
+        <List items={items} />
       </div>
     </div>
   );
--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,9 +1,32 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
 describe('App component', () => {
   test('App Renders', () => {
     render(<App />);
     expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.getByText('Find course:')).toBeInTheDocument();
+  });
+
+  test('typing in Searchbox works', () => {
+    render(<App />);
+
+    expect(screen.queryByDisplayValue(/React/)).toBeNull();
+    userEvent.type(screen.getByRole('textbox'), 'React');
+    expect(screen.getByDisplayValue(/React/)).toBeInTheDocument();
+  });
+
+  test('search filter is working', () => {
+    render(<App />);
+
+    expect(screen.getByText(/Angular/)).toBeInTheDocument();
+    expect(screen.getByText(/JavaScript/)).toBeInTheDocument();
+
+    userEvent.type(screen.getByRole('textbox'), 'script');
+
+    expect(screen.queryByDisplayValue(/Angular/)).toBeNull();
+    expect(screen.queryByText(/JavaScript/)).toBeInTheDocument();
   });
 });
